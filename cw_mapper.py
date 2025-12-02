9dd79f46a868b53bd04b4c2498de8cea13b9c6a6
Stephen Nguyen <stephennguyen@live.com.au>
2025-12-03 10:42:01 +1100

    wrote config handler and add to config method

--- cw_mapper.py
+++ cw_mapper.py
@@ -6,6 +6,7 @@ MAP_CONFIG = os.path.join(CONFIG_DIR,'mapper_config.json')
 
 DEFAULT_CONFIG = {
     "CW_VANILLA": [
+        ['CK3 Vanilla', 0],
         ['Africa Plus', 3401420817],
         ['Buffed Mongol Invasion', 2796578078],
         ['Cultures Expanded', 2829397295],
@@ -30,7 +31,7 @@ DEFAULT_CONFIG = {
     ],
 }
 
-def map_config():
+def init_map_config():
     # Initialise the default config, if none present
     os.makedirs(CONFIG_DIR,exist_ok=True)
     if not os.path.exists(MAP_CONFIG):
@@ -40,5 +41,33 @@ def map_config():
         except Exception as e:
             print('Error: {e}')
             exit(1) # Exit with an error
-    print(MAP_CONFIG)
-    pass
+
+def add_map_config(mapper_key, mapper_config):
+    # Open the config, and initialise if missing
+    try:
+        with open (MAP_CONFIG, 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError as e:
+        print(f'WARNING: {e}, config file not found in {CONFIG_DIR}. Initialising config and trying again')
+        print(f'Initialising {MAP_CONFIG} with defaults')
+        init_map_config()
+        add_map_config()
+    except json.JSONDecodeError as e:
+        print(f'ERROR: {e}, the file {MAP_CONFIG} exists but is not valid JSON or is corrupt')
+        exit(1) # Exit with an error
+
+    if mapper_key not in data:
+        data[mapper_key] = mapper_config
+    else:
+        existing_config = set((tuple(item)) for item in data[mapper_key])
+        for item in mapper_config:
+            if tuple(item) not in existing_config:
+                data[mapper_key].append(item)
+                existing_config.add(tuple(item)) # Add to existing set
+    
+    try:
+        with open(MAP_CONFIG, 'w') as f:
+            json.dump(data, f, indent=4)
+        print(f'Successfully updated {MAP_CONFIG} with {mapper_key}')
+    except Exception as e:
+        print(f'Error: {e}, error occurred while writing to config file')
