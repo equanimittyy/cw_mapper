0b82e696a2543dd777f485f82f49d5ac6f76ebf8
Stephen Nguyen <stephennguyen@live.com.au>
2025-12-03 17:24:21 +1100

    removed pysimplegui

---
+++ testing.py
@@ -0,0 +1,8 @@
+import cw_mapper
+import cw_map_checker
+
+# cw_mapper.init_map_config()
+
+# cw_map_checker.mapping_validation(*cw_map_checker.get_keys(cw_map_checker.get_cw_config()))
+
+cw_map_checker.summary()
